7bd6e68f0ddb38f0909355205fab729edf061771
Vishwajeet rai <vishwajeet.rai@leapfinance.com>
2021-04-04 16:51:56 +0530

    video server init

---
+++ server.js
@@ -0,0 +1,26 @@
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const app = require("express")();
+const http = require("http").Server(app);
+const PORT = 8000;
+const io = require("socket.io")(http, { cors: { origins: "*" } });
+
+http.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
+app.get("/", (req, res) => {
+  res.redirect(`/${uuid()}`);
+});
+
+io.on("connection", (socket) => {
+  socket.emit("connection", socket.id);
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+    socket.on("disconnect", () => {
+      socket.emit("disconnection", userId);
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+});
